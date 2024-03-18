0f4d57a68b29b09b7aa377255d1199bdbb6649fc
Leandro Doctors <ldoctors@gmail.com>
2024-03-18 22:40:56 +0100

    formatting

--- test/allentiak/challenges/raindrops/core_test.clj
+++ test/allentiak/challenges/raindrops/core_test.clj
@@ -4,12 +4,12 @@
    [expectations.clojure.test :refer [defexpect expect expecting]]))
 
 (defexpect core-should
-    (expecting "base cases"
-               (expect (= (sut/raindrops 2) "pling"))
-               (expect (= (sut/raindrops 3) "plang"))
-               (expect (= (sut/raindrops 5) "plong"))
-               (expect (= (sut/raindrops 17) "tshäng")))
+  (expecting "base cases"
+             (expect (= (sut/raindrops 2) "pling"))
+             (expect (= (sut/raindrops 3) "plang"))
+             (expect (= (sut/raindrops 5) "plong"))
+             (expect (= (sut/raindrops 17) "tshäng")))
 
-    (expecting "other base cases"
+  (expecting "other base cases"
                ;; for all integers from 1 to +infinite, except 2,3,5,and 17...
-               (expect (= (sut/raindrops 1) "blob"))))
+             (expect (= (sut/raindrops 1) "blob"))))
