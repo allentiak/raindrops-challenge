3f5144bd2b16546e380a5a09dae4eee89dcc5e17
Leandro Doctors <ldoctors@gmail.com>
2024-03-21 14:02:09 +0100

    experiment: divisible-by-many? (part 3)

--- src/allentiak/challenges/raindrops/core.clj
+++ src/allentiak/challenges/raindrops/core.clj
@@ -1,23 +1,53 @@
 (ns allentiak.challenges.raindrops.core
   (:gen-class))
 
-(def ^:private special-cases
-  {:2 "pling"
-   :3 "plang"
-   :5 "plong"
-   :17 "tshäng"})
+(defrecord SpecialCase [divisor output])
+
+(def special-cases
+  #{{:divisor 2 :output "pling"}
+    {:divisor 3 :output "plang"}
+    {:divisor 5 :output "plong"}
+    {:divisor 17 :output "tshäng"}})
+
+(comment
+  (map :divisor special-cases)
+  ;; => (17 2 3 5)
+  ,)
 
 (defn- divisible?
   "given an integer and an integer divisor, returns whether the number is divisible by the divisor"
   [n divisor]
   (zero? (mod n divisor)))
 
+(defn- divisible-by-special-case?
+  "given an integer and a special case, return a new map with a new ':divisible' key with the result of applying 'divisible?' to the inteeger and the special case"
+  [n special-case]
+  (assoc special-case :divisible (divisible? n (:divisor special-case))))
+
+(comment
+  (divisible-by-special-case? 4 (first special-cases))
+  ;; => {:divisor 17, :output "tshäng", :divisible false}
+  (divisible-by-special-case? 4 (second special-cases))
+  ;; => {:divisor 2, :output "pling", :divisible true}
+  ,)
+
 (defn- divisible-by-many?
-  "given an integer and a map of special cases '{:divisor canned-response}', returns a vector with the result of applying 'divisible?' to the integer and each of the elements of the vector"
-  [n divisors-map]
-  (let [divisors (map (comp parse-long name) (keys divisors-map))
-        ns (take (count divisors-map) (repeat n))]
-    (map divisible? ns divisors)))
+  "given an integer and a seq of SpecialCase's, returns a seq augmenting each SpecialCase with a new key ':divisible' with the result of applying 'divisible?' to the integer and the divisor"
+  [n divisors-set]
+  (let [ns (take (count divisors-set) (repeat n))]
+    (map divisible-by-special-case? ns divisors-set)))
+
+(comment
+  (assoc (first special-cases) :divisible "whatever")
+  ;; => {:divisor 17, :output "tshäng", :divisible "whatever"}
+  (let [n 17
+        my-case (first special-cases)]
+    (assoc my-case :divisible (divisible? n (:divisor my-case))))
+  ;; => {:divisor 17, :output "tshäng", :divisible true}
+  (divisible-by-many? 4 special-cases)
+  ;; => ({:divisor 17, :output "tshäng", :divisible false} {:divisor 2, :output "pling", :divisible true} {:divisor 3, :output "plang", :divisible false} {:divisor 5, :output "plong", :divisible false})
+  ,)
+
 
 (comment
   (take 3 (repeat 2))
