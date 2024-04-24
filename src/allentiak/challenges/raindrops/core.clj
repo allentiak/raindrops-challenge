29f6fdfac770ffc0d068e24639b5436380f37b1b
Leandro Doctors <ldoctors@gmail.com>
2024-04-24 18:56:42 +0200

    refactor: move 'int->base-sound'

--- src/allentiak/challenges/raindrops/core.clj
+++ src/allentiak/challenges/raindrops/core.clj
@@ -1,17 +1,6 @@
 (ns allentiak.challenges.raindrops.core
   (:gen-class))
 
-(defn- int->base-sound
-  "given an integer,
-  return its predefined sound as per the problem definition"
-  [n]
-  (condp = n
-    2 "pling"
-    3 "plang"
-    5 "plong"
-    17 "tshäng"
-    "blob"))
-
 (defn- times-divisible
   "given two integers, 'n' and 'divisor',
   return how many times is 'n' divisible by 'divisor'"
@@ -31,3 +20,14 @@
     (map (partial times-divisible n))
     (zipmap divisors)
     (filter #(pos? (val %)))))
+
+(defn- int->base-sound
+  "given an integer,
+  return its predefined sound as per the problem definition"
+  [n]
+  (condp = n
+    2 "pling"
+    3 "plang"
+    5 "plong"
+    17 "tshäng"
+    "blob"))
